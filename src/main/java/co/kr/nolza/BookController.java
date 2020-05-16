aa6edd479017d43dbbdd4db041e62b3fd62433d5
abbymark <abbymark@naver.com>
2020-05-16 22:45:53 +0900

    book_controller 작성 완료

--- src/main/java/co/kr/nolza/BookController.java
+++ src/main/java/co/kr/nolza/BookController.java
@@ -1,6 +1,8 @@
 package co.kr.nolza;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import model.book.Book_boardDto;
 
@@ -36,7 +39,7 @@ public class BookController {
 		model.addAttribute("book_indent", book_indent);
 		model.addAttribute("book_type", book_type);
 		
-		return "/board/writeForm";//뷰이름
+		return "/book/book_writeForm";//뷰이름
 	}//writeForm() end
 	
 	//Db에 글쓰기
@@ -71,6 +74,150 @@ public class BookController {
 		}
 		
 		sqlSession.insert("book_board.insertBoard",book_boardDto);
-		return "redirect:list.do";
+		return "redirect:book_list.do";
 	}//writePro() end
+	
+	//리스트
+	@RequestMapping("book_list.do")
+	public String list(Model model, String pageNum)throws NamingException, IOException{
+		
+		if(pageNum==null) {
+			pageNum="1";
+		}
+		
+		int pageSize = 10;//한페이지에 10개
+		int currentPage=Integer.parseInt(pageNum);//현재 페이지
+		
+		int startRow=(currentPage-1)*pageSize+1;//한 페이지의 시작 행
+		int endRow=currentPage*pageSize;//한 페이지의 마지막 행
+		
+		int count=0;//글 전체
+		int pageBlock=10;//1블럭당 10페이지씩 표시하려고
+		
+		count=sqlSession.selectOne("book_board.selectCount");//총 글 갯수
+		
+		int number=count-(currentPage-1)*pageSize;//글번호
+		int pageCount=count/pageSize+(count%pageSize==0?0:1);//총 페이지 객수
+		
+		int result = currentPage/10;// 2/10=10   12/10=1   22/10=2
+		int startPage=result*10+1;
+		//						0*10+1=1페이지  1*10+1=11페이지 2*10+1=21페이지
+		int endPage=startPage+pageBlock-1;
+		//						1+10-10 페이지   1+20-1=20페이지    1+30-1=30 페이지
+		
+		if(endPage>pageCount) {//에러 방지
+			endPage=pageCount;
+		}
+		
+		//*****************************************************
+		HashMap<String, Integer> map =new HashMap<String,Integer>();
+		map.put("start",startRow-1);
+		map.put("cnt",pageSize);
+		
+		List<Book_boardDto> list=sqlSession.selectList("book_board.selectList", map);
+		//*****************************************************
+		
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("startRow", startRow);
+		model.addAttribute("endRow", endRow);
+		
+		model.addAttribute("pageBlock", pageBlock);
+		model.addAttribute("count", count);
+		model.addAttribute("pageSize", pageSize);
+		model.addAttribute("pageCount", pageCount);
+		
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		
+		model.addAttribute("number", number);
+		model.addAttribute("list", list);
+		model.addAttribute("pageNum", pageNum);
+		
+		return "/book/book_list";//뷰 리턴
+	}//list() end
+	
+	//글내용보기
+	@RequestMapping("book_content.do")
+	public String content(Model model, String book_no, String pageNum)
+	throws NamingException, IOException{
+		
+		int book_no1=Integer.parseInt(book_no);
+		sqlSession.update("book_board.readCount", book_no1);//조횟수 증가
+		
+		Book_boardDto book_dto = sqlSession.selectOne("book_board.contentBoard", book_no1);
+		
+		String book_content=book_dto.getBook_content();
+		//book_content=book_content.replaceAll("\n", "<br>");
+		
+		model.addAttribute("book_content", book_content);
+		model.addAttribute("book_dto", book_dto);
+		model.addAttribute("book_no", book_no);
+		model.addAttribute("pageNum", pageNum);
+		
+		return "/book/book_content";
+	}//content() end
+	
+	//글 수정 폼
+	@RequestMapping("book_updateForm.do")
+	public ModelAndView updateForm(String book_no, String pageNum)
+			throws NamingException, IOException{
+		
+		int book_no1 = Integer.parseInt(book_no);
+		Book_boardDto book_dto = sqlSession.selectOne("book_board.contentBoard", book_no1);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("pageNum", pageNum);
+		mv.addObject("book_dto", book_dto);
+		mv.setViewName("/board/updateForm");//뷰이름
+		
+		return mv;
+	}//updateForm() end
+	
+	//DB글수정
+	@RequestMapping(value="book_updatePro.do", method=RequestMethod.POST)
+	public ModelAndView updatePro(Book_boardDto book_boardDto, String pageNum)
+			throws NamingException, IOException{
+		sqlSession.update("book_board.updateBoard", book_boardDto);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("pageNum", pageNum);
+		mv.setViewName("redirect:book_list.do");
+		
+		return mv;
+	}//updatePro() end
+	
+	//글삭제
+	@RequestMapping("book_delete.do")
+	public String delete(Model model, String book_no, String pageNum) {
+		int book_no1=Integer.parseInt(book_no);
+		sqlSession.delete("book_board.deleteBoard", book_no1);
+		
+		return "redirect:list.do";
+	}// delete() end
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
